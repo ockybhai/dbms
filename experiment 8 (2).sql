570f809154dc695dfafdab594744010508298cc3
ockybhai <146436540+ockybhai@users.noreply.github.com>
2024-05-02 14:13:43 +0530

    Add files via upload

---
+++ experiment 8 (2).sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE PROCEDURE welcome_msg (p_name IN
+VARCHAR2,salary out number)
+IS
+BEGIN
+salary:=10000;
+dbms_output.put_line ('Welcome ' || p_name);
+END;
+/
+var sal number;
+ EXEC welcome_msg ('Amrutha',:sal);
+ Welcome Amrutha
+ Print sal;
